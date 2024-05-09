fc2c0663aff02a46312016964cbeaf06ede200ed
staradrik <adrianayara12@gmail.com>
2024-05-08 20:13:29 -0500

    hexagonal architecture design

---
+++ src/app/Presentation/Components/barcode/barcode.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barcode',
+  standalone: true,
+  imports: [],
+  templateUrl: './barcode.component.html',
+  styleUrl: './barcode.component.css'
+})
+export class BarcodeComponent {
+
+}
