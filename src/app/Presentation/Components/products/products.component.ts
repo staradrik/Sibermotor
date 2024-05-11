14966f807589cae81f9fb00d9c4dbdb3e6d10afa
Velandia2022 <velandia0903@gmail.com>
2024-05-10 20:38:54 -0500

    Product search

--- src/app/Presentation/Components/products/products.component.ts
+++ src/app/Presentation/Components/products/products.component.ts
@@ -9,6 +9,7 @@ import {InputTextModule} from 'primeng/inputtext';
 import { ProductRepositoryService } from '../../../Infrastructure/Repositories/productRepository.service';
 import { Product } from '../../../Core/Services/product.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-products',
   standalone: true,
@@ -18,6 +19,7 @@ import { Router } from '@angular/router';
     ButtonModule,
     ToolbarModule,
     FileUploadModule,
+    FormsModule,
     InputTextModule
   ],
   providers: [MessageService],
@@ -28,6 +30,7 @@ export class ProductsComponent implements OnInit {
   constructor(private productService: ProductRepositoryService, private messageService: MessageService, private router: Router){}
 
   products:Product[] = [];
+  searchBox: string = "";
 
   ngOnInit() {
     this.productService.getProducts().then((data)=>{
@@ -42,6 +45,17 @@ export class ProductsComponent implements OnInit {
 
   goBarcode(){
     this.router.navigate([`/barcode`]);
+  }
+
+  searchProduct(){
+    this.productService.getProduct(this.searchBox).then((data: Product)=> {
+      if (data.description.startsWith("Error: ")){
+        this.messageService.add({severity:'error', summary:"Error", detail:data.description});
+        return;
+      }
+      this.messageService.add({severity:'success', summary:"Producto encontrado: ", detail:data.name});
+      this.router.navigate([`/product/detail/${data.barcode}`]);
+    })
   }
 
 }
